2f33566b4ab7d96313c127430eca9bc48ce32231
Ashley Lam <ashley_850819@yahoo.com>
2020-11-24 12:35:28 +1100

    added factory pattern and deleted database.py

--- src/database.py
+++
@@ -1,6 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-
-def init_db(app):
-    db = SQLAlchemy(app)
-    return db
--- src/main.py
+++ src/main.py
@@ -1,26 +1,32 @@
+from flask import Flask, jsonify  # noqa: E402
+from marshmallow.exceptions import ValidationError  # noqa: E402
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from dotenv import load_dotenv
 load_dotenv()
 
-from flask import Flask, jsonify  # noqa: E402
-from marshmallow.exceptions import ValidationError  # noqa: E402
-app = Flask(__name__)
-app.config.from_object("default_settings.app_config")
 
-from database import init_db  # noqa: E402
-db = init_db(app)
-
-from flask_marshmallow import Marshmallow  # noqa: E402
-ma = Marshmallow(app)
-
-from commands import db_commands    # noqa: E402
-app.register_blueprint(db_commands)
-
-from controllers import registerable_controllers  # noqa: E402
-
-for controller in registerable_controllers:
-    app.register_blueprint(controller)
+db = SQLAlchemy()
+ma = Marshmallow()
 
 
-@app.errorhandler(ValidationError)
-def handle_bad_request(error):
-    return (jsonify(error.messages), 400)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("default_settings.app_config")
+
+    db.init_app(app)
+    ma.init_app(app)
+
+    from commands import db_commands    # noqa: E402
+    app.register_blueprint(db_commands)
+
+    from controllers import registerable_controllers  # noqa: E402
+
+    for controller in registerable_controllers:
+        app.register_blueprint(controller)
+
+    @app.errorhandler(ValidationError)
+    def handle_bad_request(error):
+        return (jsonify(error.messages), 400)
+
+    return app
