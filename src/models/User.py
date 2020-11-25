bded8c4fe395d318095d29e92bff12409de67274
Ashley Lam <ashley_850819@yahoo.com>
2020-11-25 23:36:23 +1100

    fixed db relationship issues with imports to models.User

--- src/models/User.py
+++ src/models/User.py
@@ -1,4 +1,6 @@
 from main import db
+from models.Blog import Blog
+from models.Review import Review
 
 
 class User(db.Model):
@@ -7,8 +9,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(), nullable=False, unique=True)
     password = db.Column(db.String(), nullable=False)
-    blogs = db.relationship("Blog", backref="users", lazy="dynamic")
-    reviews = db.relationship("Review", backref="users", lazy="dynamic")
+    blogs = db.relationship(Blog, backref="users", lazy="dynamic")
+    reviews = db.relationship(Review, backref="users", lazy="dynamic")
 
     def __repr__(self):
         return f"<User {self.email}"
