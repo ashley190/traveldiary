32afb9678a6a451e77b714bfb9e7ac0e42fabfc4
Ashley Lam <ashley_850819@yahoo.com>
2020-11-26 09:15:40 +1100

    nested user schema working

--- src/models/User.py
+++ src/models/User.py
@@ -4,13 +4,13 @@ from models.Review import Review
 
 
 class User(db.Model):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(), nullable=False, unique=True)
     password = db.Column(db.String(), nullable=False)
-    blogs = db.relationship(Blog, backref="users", lazy="dynamic")
-    reviews = db.relationship(Review, backref="users", lazy="dynamic")
+    blogs = db.relationship(Blog, backref="user", lazy="dynamic")
+    reviews = db.relationship(Review, backref="user", lazy="dynamic")
 
     def __repr__(self):
         return f"<User {self.email}"
