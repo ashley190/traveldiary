cfe00915ad34a5657e3ee46a5ec66562bf5513ad
Ashley Lam <ashley_850819@yahoo.com>
2020-11-26 14:43:25 +1100

    Completed authentication and updated custom commands

--- src/commands.py
+++ src/commands.py
@@ -21,8 +21,23 @@ def drop_db():
 def seed_db():
     from models.Blog import Blog
     from models.Review import Review
+    from models.User import User
+    from main import bcrypt
     from faker import Faker
+    import random
+
+
     faker = Faker()
+    users = []
+
+    for i in range(5):
+        user = User()
+        user.email = f"test{i}@test.com"
+        user.password = bcrypt.generate_password_hash("123456").decode("utf-8")
+        db.session.add(user)
+        users.append(user)
+    
+    db.session.commit()
 
     for i in range(10):
         blog = Blog()
@@ -30,6 +45,7 @@ def seed_db():
         blog.date = faker.date_object()
         blog.location = f"{faker.city()}, {faker.country()}"
         blog.blog = faker.text()
+        blog.userid = random.choice(users).id
         db.session.add(blog)
 
     db.session.commit()
@@ -44,6 +60,7 @@ def seed_db():
         review.activity_type = faker.word()
         review.rating = faker.random_int(min=0, max=5)
         review.description = faker.text()
+        review.userid = random.choice(users).id
         db.session.add(review)
 
     db.session.commit()
