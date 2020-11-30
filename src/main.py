08c1ce29dc17c17d543e1793f9dfd72f57b0c664
Ashley Lam <ashley_850819@yahoo.com>
2020-11-30 12:01:42 +1100

    installed and created initial migration with flask_migrate

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -12,16 +13,22 @@ db = SQLAlchemy()
 ma = Marshmallow()
 bcrypt = Bcrypt()
 jwt = JWTManager()
+migrate = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("default_settings.app_config")
 
+    if app.config["ENV"] == "production":
+        from log_handlers import file_handler
+        app.logger.addHandler(file_handler)
+
     db.init_app(app)
     ma.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+    migrate.init_app(app, db)
 
     from commands import db_commands    # noqa: E402
     app.register_blueprint(db_commands)
@@ -34,5 +41,10 @@ def create_app():
     @app.errorhandler(ValidationError)
     def handle_bad_request(error):
         return (jsonify(error.messages), 400)
+
+    @app.errorhandler(500)
+    def handle_500(error):
+        app.logger.error(error)
+        return ("bad stuff", 500)
 
     return app
