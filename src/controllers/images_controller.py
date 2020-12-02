d1a62037cdb28bd9e4330cc1601cde7bcf1e298d
Ashley Lam <ashley_850819@yahoo.com>
2020-12-02 15:48:37 +1100

    updated images_controller

--- src/controllers/images_controller.py
+++ src/controllers/images_controller.py
@@ -2,7 +2,10 @@ from models.Image import Images
 from models.User import User
 from schemas.ImageSchema import image_schema
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from flask import Blueprint, request, jsonify, abort
+from flask import Blueprint, request, jsonify, abort, current_app, Response
+from pathlib import Path
+from main import db
+import boto3
 
 
 images = Blueprint("images", __name__, url_prefix = "/<int:userid>/image")
@@ -10,17 +13,69 @@ images = Blueprint("images", __name__, url_prefix = "/<int:userid>/image")
 @images.route("/", methods=["POST"])
 @jwt_required
 def create_user_image(userid):
-    if "image" in request.files:
-        image = request.files["image"]
-        image.save("uploaded_images/file_1")
-        return ("", 200)
-    return abort(400, description="No image")
+    user = User.query.filter_by(userid=get_jwt_identity())
+
+    if user.count() != 1:
+        return abort(401, description="Invalid user")
+    
+    if "image" not in request.files:
+        return abort(400, description="No image")
+
+    image = request.files["image"]
+
+    if Path(image.filename).suffix not in [".jpg", ".jpeg", ".png", ".tif", ".gif"]:
+        return abort(400, description="Invalid file type")
+
+    filename = f"{userid}{Path(image.filename).suffix}"
+    bucket = boto3.resource("s3").Bucket(current_app.config["AWS_S3_BUCKET"])
+    key = f"user_images/{filename}"
+    bucket.upload_fileobj(image, key)
+
+    if not (user[0].image):
+        new_image = Images()
+        new_image.filename = filename
+        user[0].image = new_image
+        db.session.commit()
+    
+    return ("", 200)
+    # if "image" in request.files:
+    #     image = request.files["image"]
+    #     image.save("uploaded_images/file_1")
+    #     return ("", 200)
+    # return abort(400, description="No image")
 
 @images.route("/<int:imageid>", methods=["GET"])
 def show_user_image(userid, imageid):
-    pass
+    image = Images.query.filter_by(imageid=imageid, user_id=userid).first()
+
+    if not image:
+        return abort(404, description="No image found")
+    
+    bucket = boto3.resource("s3").Bucket(current_app.config["AWS_S3_BUCKET"])
+    filename = image.filename
+    file_obj = bucket.Object(f"user_images/{filename}").get()
+
+    return Response(
+        file_obj["Body"].read(),
+        mimetype="image/*",
+        headers={"Content-Disposition": f"attachment;filename=image"}
+    )
 
 @images.route("/<int:imageid>", methods=["DELETE"])
 @jwt_required
 def delete_user_image(userid, imageid):
-    pass
+    user = User.query.filter_by(userid=get_jwt_identity()).first()
+
+    if not user:
+        return abort(401, description="Invalid user")
+    
+    if user.image:
+        bucket = boto3.resource("s3").Bucket(current_app.config["AWS_S3_BUCKET"])
+        filename = user.image.filename
+
+        bucket.Object(f"user_images/{filename}").delete()
+
+        db.session.delete(user.image)
+        db.session.commit()
+    
+    return jsonify("successfully removed")
--- src/default_settings.py
+++ src/default_settings.py
@@ -14,11 +14,25 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv("DB_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = "secret"
+    DEBUG = False
+
+    MAX_CONTENT_LENGTH = 1 * 1024 * 1024
 
     @property
     def SQLALCHEMY_DATABASE_URI(self):
         return get_from_env("DB_URI")
 
+    @property
+    def AWS_ACCESS_KEY_ID(self):
+        return get_from_env("AWS_ACCESS_KEY_ID")
+    
+    @property
+    def AWS_SECRET_ACCESS_KEY(self):
+        return get_from_env("AWS_SECRET_ACCESS_KEY")
+
+    @property
+    def AWS_S3_BUCKET(self):
+        return get_from_env("AWS_S3_BUCKET")
 
 class DevelopmentConfig(Config):
     DEBUG = True
